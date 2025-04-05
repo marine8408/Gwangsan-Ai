60d473669c83a740923c34bb5335c23b0695f293
marine8408 <killeruo2@gmail.com>
2025-04-05 17:32:27 +0900

    d git commit -m d

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def main():
 	    'service': 'address',
 	    'request': 'getcoord',
 	    'crs': 'epsg:4326',
-	    'address': '광주광역시 광산구 신가동 1306',
+	    'address': '광주광역시 광산구 신가동 1284',
 	    'format': 'json',
 	    'type': 'parcel',
 	    'key': 'AF338F49-6AAA-3F06-BD94-FB6CB6817323' }
@@ -21,18 +21,46 @@ def main():
     
     if response.status_code == 200:
         print(response.json())
+        data = response.json()
+
+        # 브이월드 서버 지오코더에서 받아온 데이타 중 좌표 x, y 값 출력
+        x = data['response']['result']['point']['x']
+        y = data['response']['result']['point']['y']
+        #print(x, y)
+
+        """"
+        # Extracting data for table
+        result_info = data['response']['result']
+        point_info = result_info['point']
+
+        df = pd.DataFrame([
+        #['Service Address', result_info['point']],
+        ['Longitude', point_info['x']],
+        ['Latitude', point_info['y']]
+        ], columns=['Key', 'Value'])
+        print(df)
+
         print("좌표추출끝")
+        """
 
 
 
-#여기부터 토지이용속성 조회
+        #여기부터 토지이용속성 조회
+        pbbox = f'{y},{x},{y},{x},EPSG:4326'    #pbbox 변수에 지오코더 좌표 값 문자열 받기
 
-    url = 'http://api.vworld.kr/ned/wfs/getLandUseWFS'
-    queryParams = '?' + urlencode({ 'key' : '86DD225C-DC5B-3B81-B9EB-FB135EEEB78C', 'domain' : 'https://refactored-rotary-phone-975v95j496w62ppqx-8501.app.github.dev/', 'typename' : 'dt_d154', 'bbox' : '35.181171572168196,126.81539362917458,35.181171572168196,126.81539362917458,EPSG:4326', 'maxFeatures' : '10', 'resultType' : 'results', 'srsName' : 'EPSG:4326', 'output' : 'text/xml; subtype=gml/2.1.2'})   
+        url = 'https://api.vworld.kr/ned/wfs/getLandUseWFS'
+        queryParams = '?' + urlencode({
+            'key' : '86DD225C-DC5B-3B81-B9EB-FB135EEEB78C',
+            'typename' : 'dt_d154',
+            'bbox' : pbbox,
+            'maxFeatures' : '10',
+            'resultType' : 'results',
+            'srsName' : 'EPSG:4326',
+            'output' : 'text/xml; subtype=gml/2.1.2'})   
 
-    request = Request(url + queryParams)    
-    request.get_method = lambda: 'GET'
-    response_body = urlopen(request).read()
-    print(response_body.decode('utf-8'))
+        request = Request(url + queryParams)    
+        request.get_method = lambda: 'GET'
+        response_body = urlopen(request).read()
+        print(response_body.decode('utf-8'))
 
 main()
